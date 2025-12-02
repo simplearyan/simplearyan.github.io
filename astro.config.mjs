2c61c7345b7a99450c6653d0cbf5ccf51bc3038d
simplearyan <aryanphone00620@gmail.com>
2025-12-02 20:00:04 +0530

    Fixed PWA Build Error 🚀 🍃

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,7 +37,8 @@ export default defineConfig({
           ]
         },
         workbox: {
-          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+          maximumFileSizeToCacheInBytes: 5000000
         }
       })
     ]
